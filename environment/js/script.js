2a85de25c3421b5f77090925f2029a3af5ed77a1
Таня Трачук <tankatrachuk@gmail.com>
2024-02-01 18:09:08 +0200

    environment

--- environment/js/script.js
+++ environment/js/script.js
@@ -1,12 +1,34 @@
-"use strict";
+// "use strict";
 
-let number = 5;debugger
+// let number = 5;debugger
 
-function logNumber(){
-    let number = 4;
-    console.log(number);
-}
+// function logNumber(){
+//     let number = 4;
+//     console.log(number);
+// }
 
-number = 6;
+// number = 6;
 
-logNumber();debugger
+// logNumber();debugger
+
+// function createCounter(){
+//     let counter =0;
+//     const myfunction =()=>{
+//         counter++;
+//         return counter;
+//     }
+//     return myfunction;
+// }
+
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+
+// console.log(c1,c2,c3);
+
+console.log([]+false-null+true);//[] -> ""
+
+console.log(2 && 1 && null && 0 && undefined);
+//&& виконується раніше чим ||
+
